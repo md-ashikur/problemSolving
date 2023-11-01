a665ac32efa28cd6435ba58f21a0b30dbb70cafd
md-ashikur <ashik76690@gmail.com>
2023-11-02 00:30:28 +0600

    star print

--- CodeForces/1 nov 2023/Shape1.c
+++ CodeForces/1 nov 2023/Shape1.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    int n;
-    scanf("%d",&n);
-    for(int i=0; i<n; i++){
-        
+    int N;
+    scanf("%d", &N);
+    int k = N;
+    for (int i = 1; i <= N; i++)
+    {
+
+        for (int j = 1; j <= k; j++)
+        {
+            printf("*");
+        }
+        k--;
+        printf("\n");
     }
+    return 0;
 }
+
+
+// Given a number N. Print a face down right angled triangle that has N rows.
+
+// For more clarification see the example below.
+
+// Input
+// Only one line containing a number N (1 ≤ N ≤ 99).
+
+// Output
+// Print the answer according to the required above.
+
+// input -> 4
+// output ->
+// ****
+// ***
+// **
+// *
