353f19993b2b35023832fbb9b8236ef5104cc979
md-ashikur <ashik76690@gmail.com>
2023-11-02 02:23:44 +0600

    sorting

--- CodeForces/1 nov 2023/shape2.c
+++ CodeForces/1 nov 2023/shape2.c
@@ -6,7 +6,6 @@ int main()
     int N;
     scanf("%d", &N);
     int s=N-1;
-    int k = 1;
     for (int i = 1; i <= N; i++)
     {
 
@@ -14,12 +13,28 @@ int main()
         {
             printf(" ");
         }
-        for (int j = 1; j <= k; j++)
+        for (int j = 1; j <= 2*i-1; j++)
         {
             printf("*");
         }
         s--;
-        k=k+2;
+     
+        printf("\n");
+
+    }
+    for (int i = N; i > 0; i--)
+    {
+
+        for (int j = 1; j <= N-i; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2*i-1; j++)
+        {
+            printf("*");
+        }
+        s++;
+
         printf("\n");
     }
     return 0;
