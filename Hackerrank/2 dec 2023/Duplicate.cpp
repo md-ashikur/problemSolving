26d44ad24348a9d198f8578f889e97a0930d2be6
md-ashikur <ashik76690@gmail.com>
2023-12-03 00:16:25 +0600

    Duplicate solved

--- Hackerrank/2 dec 2023/Duplicate.cpp
+++ Hackerrank/2 dec 2023/Duplicate.cpp
@@ -1,8 +1,36 @@
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+    long long n;
+    cin >> n;
+    long long a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
 
+    bool flag = false;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j <= n; j++)
+        {
+            if (a[i] == a[j])
+            {
+                flag = true;
+            }
+        }
+    }
+    if (flag == true)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
 
     return 0;
 }
