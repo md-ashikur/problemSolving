877a3a0c5fae363e5bcd3c682a5bdf35e64a9cde
md-ashikur <ashik76690@gmail.com>
2024-03-19 23:22:00 +0600

    Codeforces Round 935

---
+++ CodeForces/Div 3 Contest/Codeforces Round 935/SettingupCamp.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int a, b=7, c;
+        // cin >> a >> b >> c;
+        int tent1=0, tent2=0,tent3=0;
+        if (b < 3)
+        {
+            if (b%3==0){
+
+            }
+        }
+
+        cout<<b%3   ;
+    }
+
+    return 0;
+}
---
+++ CodeForces/Div 3 Contest/Codeforces Round 935/fireworks.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+   int t;
+   cin>>t;
+   while(t--){
+   long long int a, b, c;
+   cin>>a>>b>>c;
+   
+   cout<<(c/a)+(c/b)+2<<endl;
+
+   }
+    return 0;
+}
